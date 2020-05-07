a52eaf42ce76e017e82c50180823b9371f453c57
toruMatsuyama <matsuyt.2020@gmail.com>
2020-05-07 14:00:35 +0900

    Q005 データクラスと様々な集計

--- src/main/java/q005/Q005.java
+++ src/main/java/q005/Q005.java
@@ -1,5 +1,22 @@
 package q005;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+
+import static q005.WorkData.PositionType;
+import static q005.WorkData.PositionType.Butyo;
+import static q005.WorkData.PositionType.Ippan;
+import static q005.WorkData.PositionType.Katyo;
+
 /**
  * Q005 データクラスと様々な集計
  *
@@ -30,5 +47,109 @@ T-7-30002: xx時間xx分
 （省略）
  */
 public class Q005 {
+
+    public static void main(String[] args) {
+        List<WorkData> workDataList = getData();
+        sumTimePosition(workDataList);
+        sumTimePCode(workDataList);
+        sumTimeMember(workDataList);
+    }
+
+    private static void sumTimePosition(List<WorkData> workDataList) {
+        Map<String, Integer> positionData = new LinkedHashMap<>();
+        int sumTimeButyo = 0;
+        int sumTimeKatyo = 0;
+        int sumTimeIppan = 0;
+
+        for (WorkData workData : workDataList) {
+            switch (Objects.requireNonNull(PositionType.getByName(workData.getPosition()))) {
+                case Butyo:
+                    sumTimeButyo += workData.getWorkTime();
+                    break;
+                case Katyo:
+                    sumTimeKatyo += workData.getWorkTime();
+                    break;
+                case Ippan:
+                    sumTimeIppan += workData.getWorkTime();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + workData.getPosition());
+            }
+        }
+
+        positionData.put(Butyo.getName(), sumTimeButyo);
+        positionData.put(Katyo.getName(), sumTimeKatyo);
+        positionData.put(Ippan.getName(), sumTimeIppan);
+
+        printData(positionData);
+    }
+
+    private static void sumTimePCode(List<WorkData> workDataList) {
+        Map<String, Integer> pCodeData = new HashMap<>();
+
+        for (WorkData workData : workDataList) {
+            pCodeData.merge(workData.getpCode(), workData.getWorkTime(), Integer::sum);
+        }
+
+        printData(pCodeData);
+    }
+
+    private static void sumTimeMember(List<WorkData> workDataList) {
+        Map<String, Integer> memberData = new TreeMap<>();
+
+        for (WorkData workData : workDataList) {
+            memberData.merge(workData.getNumber(), workData.getWorkTime(), Integer::sum);
+        }
+
+        printData(memberData);
+    }
+
+    private static void printData(Map<String, Integer> dataList) {
+        for (Map.Entry<String, Integer> entry : dataList.entrySet()) {
+            System.out.println(entry.getKey() + ": " + subTime(entry.getValue()));
+        }
+    }
+
+    private static String subTime(int sec) {
+        int minutes = sec / 60;
+        return minutes / 60 + "時間" + minutes % 60 + "分";
+    }
+
+    private static InputStream openDataFile() {
+        return Q005.class.getResourceAsStream("data.txt");
+    }
+
+    public static List<WorkData> getData() {
+        List<WorkData> workDataList = new ArrayList<>();
+        String line;
+        int index = 0;
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(openDataFile()))) {
+            while ((line = br.readLine()) != null) {
+                if (index == 0) {
+                    index += 1;
+                    continue;
+                }
+                String[] data = line.split(",");
+                workDataList.add(mapper(data));
+                index += 1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return workDataList;
+    }
+
+    private static WorkData mapper(String[] data) {
+        WorkData workData = new WorkData();
+        workData.setNumber(data[0]);
+        workData.setDepartment(data[1]);
+        workData.setPosition(data[2]);
+        workData.setpCode(data[3]);
+        workData.setWorkTime(Integer.parseInt(data[4]));
+        return workData;
+    }
+
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 2時間 00分
--- src/main/java/q005/WorkData.java
+++ src/main/java/q005/WorkData.java
@@ -19,4 +19,69 @@ public class WorkData {
 
     /** 作業時間(分) */
     private int workTime;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getpCode() {
+        return pCode;
+    }
+
+    public void setpCode(String pCode) {
+        this.pCode = pCode;
+    }
+
+    public int getWorkTime() {
+        return workTime;
+    }
+
+    public void setWorkTime(int workTime) {
+        this.workTime = workTime;
+    }
+
+    protected enum PositionType {
+        Butyo("部長"),
+        Katyo("課長"),
+        Ippan("一般");
+
+        private final String name;
+
+        PositionType(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public static PositionType getByName(String name) {
+            for (PositionType positionType : PositionType.values()) {
+                if (positionType.getName().equals(name)) {
+                    return positionType;
+                }
+            }
+            return null;
+        }
+    }
 }
